1110f9f64c5bf06c64a9a063356ddb0c78a0cc0d
Kattis91 <katya.svedmark@gmail.com>
2023-11-04 12:06:43 +0000

    Initial commit

---
+++ with_travel_in_mind_api/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for with_travel_in_mind_api project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'with_travel_in_mind_api.settings')
+
+application = get_asgi_application()
