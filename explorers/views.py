0803536b04c79719a29ad430e219f939ae60f94f
Kattis91 <katya.svedmark@gmail.com>
2023-11-04 15:43:38 +0000

    Add a view for ExplorerList

--- explorers/views.py
+++ explorers/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Explorer
 
-# Create your views here.
+class ExplorerList(APIView):
+    """
+    List all eplorers
+    No Create view (post method), as profile creation handled by django signals
+    """
+    def get(self, request):
+        explorers = Explorer.objects.all()
+      
