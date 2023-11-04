901a8cb212b37a994c90821a5449f45b33dd53f5
Kattis91 <katya.svedmark@gmail.com>
2023-11-04 19:33:47 +0000

    Add is_owner field to ExplorerSerializer

--- explorers/serializers.py
+++ explorers/serializers.py
@@ -3,10 +3,18 @@ from .models import Explorer
 
 class ExplorerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        """
+        Method to check if the authenticated user is the explorer's owner
+        """
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Explorer
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'bio', 'image', 'region', 'dream_destination'
+            'bio', 'image', 'region', 'dream_destination', 'is_owner'
         ]
