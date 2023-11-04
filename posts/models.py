ad87ebb1ce2fc7fce51113ac5f383fb571bdb246
Kattis91 <katya.svedmark@gmail.com>
2023-11-04 23:17:38 +0000

    Correct the typo

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Post(models.model):
+class Post(models.Model):
     """
     Post model, related to 'owner', i.e. a User instance
     """
