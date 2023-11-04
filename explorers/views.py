4ca56e713ec4ef985e4c9bb71a28363c73a3fccc
Kattis91 <katya.svedmark@gmail.com>
2023-11-04 17:22:28 +0000

    Create ExplorerDetail view

--- explorers/views.py
+++ explorers/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Explorer
@@ -11,4 +12,19 @@ class ExplorerList(APIView):
     def get(self, request):
         explorers = Explorer.objects.all()
         serializer = ExplorerSerializer(explorers, many=True)
+        return Response(serializer.data)
+
+
+class ExplorerDetail(APIView):
+    serializer_class = ExplorerSerializer
+    def get_object(self, pk):
+        try:
+            explorer = Explorer.objects.get(pk=pk)
+            return explorer
+        except Explorer.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        explorer = self.get_object(pk)
+        serializer = ExplorerSerializer(explorer)
         return Response(serializer.data)
