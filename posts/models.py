3495a46d5bc7f6ee40dd965e17524dc2c87d8f7c
Kattis91 <katya.svedmark@gmail.com>
2023-11-04 20:34:54 +0000

    Add a new app posts. Create a post model

---
+++ posts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.model):
+    """
+    Post model, related to 'owner', i.e. a User instance
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_image_hsxgdj', blank=True
+    )
+    country = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
