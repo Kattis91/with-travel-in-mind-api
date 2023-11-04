f72d14e934f8ced2a3595b66faa8df5fe33ac567
Kattis91 <katya.svedmark@gmail.com>
2023-11-04 23:30:15 +0000

    Add urlpattern for posts.urls

--- with_travel_in_mind_api/urls.py
+++ with_travel_in_mind_api/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('explorers.urls')),
+    path('', include('posts.urls')),
 ]
