7030de35b9b62f8c9e1939949df6c64bf7c06f30
Kattis91 <katya.svedmark@gmail.com>
2023-11-05 22:52:49 +0000

    Add extra fields to the PostList and PostDetail views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import generics, permissions
 from with_travel_in_mind_api.permissions import IsOwnerOrReadOnly
 from .models import Post
@@ -10,7 +11,11 @@ class PostList(generics.ListCreateAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True),
+        bookmarks_count=Count('bookmarks', distinct=True)
+    ).order_by('-created_at')
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -22,4 +27,8 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """    
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True),
+        bookmarks_count=Count('bookmarks', distinct=True)
+    ).order_by('-created_at')
