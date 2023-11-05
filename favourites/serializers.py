d0ec673f3d90509f7b8fcc94d7d209de208a8282
Kattis91 <katya.svedmark@gmail.com>
2023-11-05 19:17:06 +0000

    Create Favourite Serializer

---
+++ favourites/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from favourites.models import Favourite
+from django.db import IntegrityError
+
+
+class FavouriteSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    favorited_name = serializers.ReadOnlyField(source='favorited.username')
+
+    class Meta:
+        model = Favourite
+        fields = [
+            'id', 'owner', 'created_at', 'favorited', 'favorited_name'
+        ]
+
+    def create(self, validated_data):
+        
+        post = validated_data.get('post')
+
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
