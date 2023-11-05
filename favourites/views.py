9e92c751ae48fe07ad995c7bfac6bff3cffcbf47
Kattis91 <katya.svedmark@gmail.com>
2023-11-05 19:17:54 +0000

    Create FavouriteList and FavouriteDetails generic views

--- favourites/views.py
+++ favourites/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from with_travel_in_mind_api.permissions import IsOwnerOrReadOnly
+from favourites.models import Favourite
+from favourites.serializers import FavouriteSerializer
 
-# Create your views here.
+
+class FavouriteList(generics.ListCreateAPIView):
+    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FavouriteSerializer
+    queryset = Favourite.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FavouriteDetail(generics.RetrieveDestroyAPIView):
+  
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FavouriteSerializer
+    queryset = Favourite.objects.all()
