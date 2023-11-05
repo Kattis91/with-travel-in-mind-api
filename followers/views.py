04ec8529dff69fe0c4d8461e0831a021bf85dfec
Kattis91 <katya.svedmark@gmail.com>
2023-11-05 14:58:20 +0000

    Create a FollowerDetail generic view

--- followers/views.py
+++ followers/views.py
@@ -12,3 +12,10 @@ class FollowerList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+  
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
