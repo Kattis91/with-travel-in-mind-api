f02b59aaf37e09ae555ab5afce7eb378b850d08a
Kattis91 <katya.svedmark@gmail.com>
2023-11-05 17:41:24 +0000

    Add a 'ceate'eunction to handle duplication errors

--- bookmarks/serializers.py
+++ bookmarks/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from bookmarks.models import Bookmark
+from django.db import IntegrityError
 
 
 class BookmarkSerializer(serializers.ModelSerializer):
@@ -9,3 +10,14 @@ class BookmarkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bookmark
         fields = ['id', 'created_at', 'owner', 'post']
+
+     def create(self, validated_data):
+        
+        post = validated_data.get('post')
+
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
