2faec5001534b83a0cbf7ed6407f55dea95842ce
Kattis91 <katya.svedmark@gmail.com>
2023-11-05 18:53:57 +0000

    Create Favourite Model (this commit should come before the last migration commit)

--- favourites/models.py
+++ favourites/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Favourite(models.Model):
+    """
+    Model for favourites (explorers you like extra)
+    """
+    owner = models.ForeignKey(
+        User, related_name='favoriting', on_delete=models.CASCADE)
+    favorited = models.ForeignKey(
+        User, related_name='favorited', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'favorited']
+
+    def __str__(self):
+        return f'{self.owner} {self.favorited}'
