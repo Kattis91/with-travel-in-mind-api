9b1991f4613b858477c1db9ba26f7e6c6cb963ef
Kattis91 <katya.svedmark@gmail.com>
2023-11-05 14:28:23 +0000

    Add a method to prevent users from creating duplicate likes

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from likes.models import Like
+from django.db import IntegrityError
 
 
 class LikeSerializer(serializers.ModelSerializer):
@@ -8,3 +9,17 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        """
+        A method to prevent users from
+        creating duplicate likes
+        """
+        post = validated_data.get('post')
+
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
