7ed40bdbe406ded3e49e31e41cb31638b0f61c63
Kattis91 <katya.svedmark@gmail.com>
2023-11-05 14:57:25 +0000

    Create a FollowerList generic view

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from with_travel_in_mind_api.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
