427a1fc2ccd655bed7d1451579ac4dfc8f94c238
Kattis91 <katya.svedmark@gmail.com>
2023-11-06 19:18:16 +0000

    Add comments to FavouriteList and FavouriteDetail views

--- favourites/views.py
+++ favourites/views.py
@@ -5,7 +5,11 @@ from favourites.serializers import FavouriteSerializer
 
 
 class FavouriteList(generics.ListCreateAPIView):
-    
+    """ 
+    List all favourites
+    Make it possible for users to add another users to
+    favourites when logged in.
+    """        
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = FavouriteSerializer
     queryset = Favourite.objects.all()
@@ -15,7 +19,10 @@ class FavouriteList(generics.ListCreateAPIView):
 
 
 class FavouriteDetail(generics.RetrieveDestroyAPIView):
-  
+    """
+    Retrieve a favourite.
+    Destroy (remove 'favorite-badge' if owner)
+    """    
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = FavouriteSerializer
     queryset = Favourite.objects.all()
