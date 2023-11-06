2f67b6064006d5a41c71357860510a37ddc94d7d
Kattis91 <katya.svedmark@gmail.com>
2023-11-06 19:27:17 +0000

    Add a coment to CurrentUserSerializer

--- with_travel_in_mind_api/serializers.py
+++ with_travel_in_mind_api/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
+    """
+    Serializer for the CurrentUser.
+    Extends the UserDetailsSerializer from 'dj_rest_auth' 
+    to include 'explorer_id' and 'explorer_image' fields.
+    """
     explorer_id = serializers.ReadOnlyField(source='explorer.id')
     explorer_image = serializers.ReadOnlyField(source='explorer.image.url')
 
