fbf37351b53dfd237706662f12cae221fd0f6a71
Kattis91 <katya.svedmark@gmail.com>
2023-11-06 19:08:49 +0000

    Add comments to FollowerList and FollowerDetail views

--- followers/views.py
+++ followers/views.py
@@ -5,17 +5,28 @@ from followers.serializers import FollowerSerializer
 
 
 class FollowerList(generics.ListCreateAPIView):
-    
+    """ 
+    List all followers
+    Make it possible for users to follow another 
+    users when logged in.
+    """    
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = FollowerSerializer
     queryset = Follower.objects.all()
 
     def perform_create(self, serializer):
+        """ 
+        Method that helps us associate the current 
+        logged in user with a follower.
+        """
         serializer.save(owner=self.request.user)
 
 
 class FollowerDetail(generics.RetrieveDestroyAPIView):
-  
+    """
+    Retrieve a follower.
+    Destroy (unfollow someone if owner)
+    """  
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = FollowerSerializer
     queryset = Follower.objects.all()
