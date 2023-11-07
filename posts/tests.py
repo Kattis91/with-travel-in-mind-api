6fd92416c9edf858bc0a768f79ddbd4aecd3d0a4
Kattis91 <katya.svedmark@gmail.com>
2023-11-07 20:23:13 +0000

    Add PostDetailViewTests

--- posts/tests.py
+++ posts/tests.py
@@ -25,3 +25,57 @@ class PostListViewTests(APITestCase):
         response = self.client.post('/posts/', {'title': 'My test post'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        kattis = User.objects.create_user(username='kattis', password='pass1234')
+        misha = User.objects.create_user(username='misha', password='pass1234')
+        Post.objects.create(
+            owner=kattis, 
+            title='My test post', 
+            description='kattis first post'
+        )
+        Post.objects.create(
+            owner=misha, 
+            title='My first post', 
+            description='mishas first post'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'My test post')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/25/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_logged_in_user_can_update_own_post(self):
+        self.client.login(username='kattis', password='pass1234')
+        response = self.client.put('/posts/1/', {'title': 'My new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'My new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_cannot_update_others_post(self):
+        self.client.login(username='misha', password='pass1234')
+        response = self.client.put('/posts/1/', {'title': 'My new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_out_user_cannot_update_any_posts(self):
+        response = self.client.put('/posts/1/', {'title': 'My new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_in_user_can_delete_own_post(self):
+        self.client.login(username='kattis', password='pass1234')
+        response = self.client.delete('/posts/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_logged_in_user_cannot_delete_others_post(self):
+        self.client.login(username='misha', password='pass1234')
+        response = self.client.delete('/posts/1/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_out_user_cannot_delete_any_posts(self):
+        response = self.client.delete('/posts/1/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
