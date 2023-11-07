c005e29255157fc2e21ebe294c7d2a3008b5cf40
Kattis91 <katya.svedmark@gmail.com>
2023-11-07 19:48:41 +0000

    Add PostListViewTests (automated testing)

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='kattis', password='pass1234')
+
+    def test_can_list_posts(self):
+        kattis = User.objects.get(username='kattis')
+        Post.objects.create(owner=kattis, title='My test post')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='kattis', password='pass1234')
+        response = self.client.post('/posts/', {'title': 'My test post'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_logged_out_user_cannot_create_post(self):
+        response = self.client.post('/posts/', {'title': 'My test post'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
