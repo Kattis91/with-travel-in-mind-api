ee1fc65bf308343089877efcd1ba1f049cd0c539
Kattis91 <katya.svedmark@gmail.com>
2023-11-15 22:21:58 +0000

    Add 'region' field and rename 'country' to 'location'

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,17 @@ class Post(models.Model):
     """
     Post model, related to 'owner', i.e. a User instance
     """
+
+    REGION_CHOICES = (
+        ('Asia', 'Asia'),
+        ('Africa', 'Africa'),
+        ('North America', 'North America'),
+        ('South America', 'South America'),
+        ('Antarctica', 'Antarctica'),
+        ('Europe', 'Europe'),
+        ('Australien', 'Australien')
+    )
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,7 +25,10 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_image_hsxgdj', blank=True
     )
-    country = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    region = models.CharField(
+        max_length=50, choices=REGION_CHOICES, default='Europe'
+    )
 
     class Meta:
         ordering = ['-created_at']
