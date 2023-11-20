66460764a21f482efcb629925cfa8f99b884887f
Kattis91 <katya.svedmark@gmail.com>
2023-11-20 15:43:05 +0000

    Add CommentLikeList and CommentLikeDetail views

--- commentlikes/views.py
+++ commentlikes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from with_travel_in_mind_api.permissions import IsOwnerOrReadOnly
+from commentlikes.models import CommentLike
+from commentlikes.serializers import CommentLikeSerializer
 
-# Create your views here.
+
+class CommentLikeList(generics.ListCreateAPIView):
+    """
+    List comment likes.
+    Make it possible to like a comment when logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = CommentLikeSerializer
+    queryset = CommentLike.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentLikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a comment like. Delete it if you're the owner.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentLikeSerializer
+    queryset = CommentLike.objects.all()
