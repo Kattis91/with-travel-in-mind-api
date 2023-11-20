030ce11a3238ee85b9e7c66ee48ae68d7917c026
Kattis91 <katya.svedmark@gmail.com>
2023-11-20 15:41:26 +0000

    Create CommentLike model

--- commentlikes/models.py
+++ commentlikes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from comments.models import Comment
 
-# Create your models here.
+
+class CommentLike(models.Model):
+    """
+    Like model, related to User and Comment
+    Enable users to like the comments
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        Comment, related_name='commentlikes', on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['owner', 'comment']
+
+    def __str__(self):
+        if self.post:
+            return f'{self.owner} {self.comment}'
